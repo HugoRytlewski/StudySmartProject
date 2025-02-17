f565c6a2d0b0cfe6eeb60f32618eac49c14dd45c
hugoRytlewski <hugorytlewski100@gmail.com>
2025-02-17 09:32:17 +0100

    Suppression de la dépendance symfony/maker-bundle dans composer.json et des références associées dans bundles.php et symfony.lock

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,4 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
 ];
