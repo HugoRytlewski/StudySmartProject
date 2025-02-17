69dabff921bedac8e7a6357157fd5d43681a0416
hugoRytlewski <hugorytlewski100@gmail.com>
2025-02-17 09:48:14 +0100

    Ajout de la configuration Webpack pour gérer les assets et création du fichier package.json pour les dépendances du projet

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+    .enableSassLoader()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction());
+
+module.exports = Encore.getWebpackConfig();
