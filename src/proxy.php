54569719dd6f76f78f3fee1bb9de089e36e538df
hugo <hugorytlewski10@gmail.com>
2025-03-22 15:07:05 +0100

    Update environment configuration to development mode and simplify URL fetching in proxy.php

--- src/proxy.php
+++ src/proxy.php
@@ -1,26 +1,5 @@
 <?php
-if (empty($_GET['url'])) {
-    http_response_code(400); 
-    echo 'Error: URL parameter is missing or empty.';
-    exit;
-}
-
 $url = $_GET['url'];
-
-// Validar que la URL sea válida
-if (!filter_var($url, FILTER_VALIDATE_URL)) {
-    http_response_code(400);
-    echo 'Error: Invalid URL.';
-    exit;
-}
-
-$response = @file_get_contents($url);
-
-if ($response === false) {
-    http_response_code(555); 
-    echo 'Error: Unable to fetch the URL.';
-    exit;
-}
-
+$response = file_get_contents($url);
 header('Content-Type: text/calendar');
 echo $response;
