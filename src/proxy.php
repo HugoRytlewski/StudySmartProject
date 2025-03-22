c44f544730a3ef30fe6f54e5db1b27bf9ef3733c
hugo <hugorytlewski10@gmail.com>
2025-03-22 16:10:48 +0100

    Add URL validation and error handling in proxy.php

--- src/proxy.php
+++ src/proxy.php
@@ -1,5 +1,16 @@
 <?php
-$url = $_GET['url'];
-$response = file_get_contents($url);
-header('Content-Type: text/calendar');
-echo $response;
+if (isset($_GET['url']) && filter_var($_GET['url'], FILTER_VALIDATE_URL)) {
+    $url = $_GET['url'];
+    $response = @file_get_contents($url);
+
+    if ($response === false) {
+        // Gérer l'erreur si la récupération échoue
+        echo "Erreur lors de la récupération des données.";
+    } else {
+        header('Content-Type: text/calendar');
+        echo $response;
+    }
+} else {
+    echo "URL invalide.";
+}
+?>
